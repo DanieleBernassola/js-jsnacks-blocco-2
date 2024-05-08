d13668cc180cec3e481691898d44042b52918ca9
DanieleBernassola <danielebernassola02@gmail.com>
2024-05-08 18:20:37 +0200

    [UPD] Added solution

--- snack-a/js/main.js
+++ snack-a/js/main.js
@@ -1,2 +1,26 @@
 'use strict';
 
+// Inizializzazioni
+const elementi = [];
+let elemento;
+let i = 0;
+
+// Funzione che conta gli elementi presenti nell'array
+function contaElementi(){
+    while (elemento !== 'stop'){
+        elemento = prompt('Inserisci un elemento');
+        elementi.push(elemento);
+        i++;
+        if (elemento == 'stop'){
+            i--;
+            elementi.pop(elemento);
+        }
+    }
+    return elementi;
+}
+
+// Chiamata della funzione
+contaElementi();
+// Output
+console.log('Questi sono gli elementi inseriti:', elementi);
+console.log('Il numero di elementi presenti è:', i);
